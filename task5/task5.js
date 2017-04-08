035e25b4b69b782b0279d98e7c37f8f48cb57b69
466300750 <466300750@qq.com>
2017-04-08 22:09:25 +0800

    last modify

--- task5/task5.js
+++ task5/task5.js
@@ -10,6 +10,15 @@ function addLoadEvent(func) {
 	}
 }
 
+function create() {
+	var ul = document.querySelector("ul");
+	for(var i=0; i<60; i++) {
+		var newLi = document.createElement("li");
+		newLi.style.height = Math.floor(Math.random()*(100-10)+10) + "px";
+		ul.appendChild(newLi);
+	}
+}
+
 function handleInput() {
 	var textNode = document.getElementById("aqi-input");
 	var text = textNode.value;
@@ -25,96 +34,203 @@ function handleInput() {
 
 function leftIn() {
 	var textNum = handleInput();
-	var div = document.getElementsByTagName("div").item(0);
+	var ul = document.querySelector("ul");
 	if(typeof textNum === "number") {		
-		if(div.childNodes.length > 60) {
-			alert("The length of the queue cannot be more than 60");
+		if(ul.childNodes.length > 60) {
+			alert("The length of the ul cannot be more than 60");
 			return;
 		} else {
-			var span = document.createElement("span");
-			span.className = "number";
-			span.style.height = textNum+"px";
-			div.insertBefore(span,div.firstElementChild);
+			var newLi = document.createElement("li");
+			newLi.style.height = textNum+"px";
+			ul.insertBefore(newLi,ul.firstElementChild);
 		}
 	}			
 }
 
 function rightIn() {
 	var textNum = handleInput();
-	var div = document.getElementsByTagName("div").item(0);
+	var ul = document.querySelector("ul");
 	if(typeof textNum === "number") {		
-		if(div.childNodes.length > 60) {
-			alert("The length of the queue cannot be more than 60");
+		if(ul.childNodes.length > 60) {
+			alert("The length of the ul cannot be more than 60");
 			return;
 		} else {
-			var span = document.createElement("span");
-			span.className = "number";
-			span.style.height = textNum+"px";
-			div.appendChild(span);
+			var newLi = document.createElement("li");
+			newLi.style.height = textNum+"px";
+			ul.appendChild(newLi);
 		}
 	}				
 }
 
 
 function leftOut() {
-	var div = document.getElementsByTagName("div").item(0);
-	var first = div.firstElementChild;
-	var text = first.style.height.slice(0, -2);
-	alert(text);
-	div.removeChild(div.firstElementChild);
+	var input = document.querySelector("input"),
+		ul = document.querySelector("ul"),
+		lis = ul.children;
+	if(lis[0]) {
+		input.value = parseInt(lis[0].style.height);
+        ul.removeChild(lis[0]);
+        alert("删除的元素为：" + input.value);
+    } else {
+        alert("队列中没有元素，无法删除");
+    }
+    input.focus();
 }
 
 function rightOut() {
-	var div = document.getElementsByTagName("div").item(0);
-	var last = div.lastElementChild;
-	var text = last.style.height.slice(0. -2);
-	alert(text);
-	div.removeChild(div.lastElementChild);
+	var input = document.querySelector("input"),
+		ul = document.querySelector("ul");
+		lis = ul.children;
+	if(lis[0]) {
+		var lastChild = ul.lastElementChild;
+		input.value = parseInt(lastChild.style.height);
+        ul.removeChild(lastChild);
+        alert("删除的元素为：" + input.value);
+    } else {
+        alert("队列中没有元素，无法删除");
+    }
+    input.focus();
 }
 
 function deleteEle(event) {
-	var div = document.getElementsByTagName("div").item(0);
-	if(event.target.nodeName.toLowerCase() === "span") {
-		div.removeChild(event.target);
+	var ul = document.querySelector("ul");
+	if(event.target.nodeName.toLowerCase() === "li") {
+		ul.removeChild(event.target);
 	}
+
+	//如果是在每个li上添加listener则可以通过下面的语句删除
+	// this.parentNode.removeChild(this);  
 }
 
 
 function bubbleSort() {
-	var numbers = document.getElementsByClassName("number");
+	var numbers = document.querySelector("ul").children;
 	var len = numbers.length;
 	if(len<2) return;
 	var arr = [];
 	for(var k=0; k<len; k++) {
 		arr.push(parseInt(numbers[k].style.height.slice(0, -2)));
 	}
-	for(var i=len; i>0; i--) {
-		for(var j=0; j<i-1; j++) {
-			if(arr[j] > arr[j+1]) {
-				var tmp = arr[j];
-				arr[j] = arr[j+1];
-				arr[j+1] = tmp;
-				numbers[j].style.height = arr[j]+"px";
-				numbers[j+1].style.height = arr[j+1]+"px";
-			}
+	for(var j=0; j<len; j++) {
+		if(arr[j] > arr[j+1]) {
+			var tmp = arr[j];
+			arr[j] = arr[j+1];
+			arr[j+1] = tmp;
 		}
+	}
+	for(k=0; k<len; k++) {
+		numbers[k].style.height = arr[k]+"px";
 	}
 }
 
-function addListener() {
-	var div = document.getElementsByTagName("div").item(0);
-	var leftInBtn = document.getElementById("leftin");
-	var rightInBtn = document.getElementById("rightin");
-	var leftOutBtn = document.getElementById("leftout");
-	var rightOutBtn = document.getElementById("rightout");
-	var sort = document.getElementById("sort");
+function pai() {
+	var resetBtn = document.getElementById("reset"),
+		ul = document.querySelector("ul"),
+		lis = ul.children;
+		tmpLis = ul.innerHTML,
+		i = 0, j = 0, timer;
+	addHandler(resetBtn, "click", function(){
+		clearInterval(timer);
+		ul.innerHTML = tmpLis;
+		allowBtn();
+	});
+	preventBtn();
+	var timer = setInterval(function() {
+		if(i < lis.length) {
+			if(j < lis.length-i-1) {
 
-	div.addEventListener("click", deleteEle, false);
-	leftInBtn.addEventListener("click", leftIn, false);
-	rightInBtn.addEventListener("click", rightIn, false);
-	leftOutBtn.addEventListener("click", leftOut, false);
-	rightOutBtn.addEventListener("click", rightOut, false);
-	sort.addEventListener("click", bubbleSort, false);
+				//为了让相比较的两个都改变颜色
+				if (j < lis.length - i - 2) {
+                    lis[j].className = "";
+                    lis[j + 2].className = "current";
+                }
+
+				if(lis[j].offsetHeight > lis[j+1].offsetHeight) {
+					var temp = lis[j].style.height;
+                    lis[j].style.height = lis[j + 1].style.height;
+                    lis[j + 1].style.height = temp;
+				}
+				j++;
+			} else {
+				//由于相邻两个都改变了颜色，因此需要重置
+				if(j - 1 > 0) {
+                   lis[j - 1].className = "";
+                }
+
+				i++;
+				j = 0;
+				lis[lis.length - i].className = "finish";
+			}
+		} else {
+			clearInterval(timer);
+			allowBtn();
+		}
+	}, "100");
+}
+
+function preventBtn() {
+	var ul = document.querySelector("ul"),
+		buttons = document.querySelectorAll("button"),
+		leftInBtn = buttons[0],
+		rightInBtn = buttons[1],
+		leftOutBtn = buttons[2],
+		rightOutBtn = buttons[3],
+		createBtn = buttons[4],
+		sortBtn = buttons[5];
+
+	leftInBtn.disabled = "disabled";
+	rightInBtn.disabled = "disabled";
+	leftOutBtn.disabled = "disabled";
+	rightOutBtn.disabled = "disabled";
+	createBtn.disabled = "disabled";
+	sortBtn.disabled = "disabled";
+}
+
+function allowBtn() {
+	var ul = document.querySelector("ul"),
+		buttons = document.querySelectorAll("button"),
+		leftInBtn = buttons[0],
+		rightInBtn = buttons[1],
+		leftOutBtn = buttons[2],
+		rightOutBtn = buttons[3],
+		createBtn = buttons[4],
+		sortBtn = buttons[5];
+
+	leftInBtn.disabled = "";
+	rightInBtn.disabled = "";
+	leftOutBtn.disabled = "";
+	rightOutBtn.disabled = "";
+	createBtn.disabled = "";
+	sortBtn.disabled = "";
+}
+
+function addListener() {
+	var ul = document.querySelector("ul"),
+		buttons = document.querySelectorAll("button"),
+		leftInBtn = buttons[0],
+		rightInBtn = buttons[1],
+		leftOutBtn = buttons[2],
+		rightOutBtn = buttons[3],
+		createBtn = buttons[4],
+		sortBtn = buttons[5];
+
+	addHandler(ul, "click", deleteEle);
+	addHandler(leftInBtn, "click", leftIn);
+	addHandler(rightInBtn, "click", rightIn);
+	addHandler(leftOutBtn, "click", leftOut);
+	addHandler(rightOutBtn, "click", rightOut);
+	addHandler(createBtn, "click", create);
+	addHandler(sortBtn, "click", pai);
+}
+
+function addHandler(element,type,handler) {
+    if(element.addEventListener){
+        element.addEventListener(type,handler);
+    }else if(element.attachEvent){
+        element.attachEvent("on"+type,handler);
+    }else{
+        element["on"+type] = handler;
+    }
 }
 
 addLoadEvent(addListener);
